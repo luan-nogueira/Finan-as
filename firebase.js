435c37c1e32acdca50078042af6613bc45e6cf15
LuanNogueira <166650037+luan-nogueira@users.noreply.github.com>
2026-03-06 21:53:14 -0300

    firebase.js
    Updated Firebase SDK imports to use specific version URLs and initialized Firestore and Auth services.

--- firebase.js
+++ firebase.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAA9oMvrsKbsixFBgJ5MlZuHdriVBcG4hA",
   authDomain: "financas-4f348.firebaseapp.com",
   projectId: "financas-4f348",
   storageBucket: "financas-4f348.firebasestorage.app",
   messagingSenderId: "521022969270",
-  appId: "1:521022969270:web:aa68de175296674d35dd17",
-  measurementId: "G-1ENMQQ30RZ"
+  appId: "1:521022969270:web:aa68de175296674d35dd17"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
